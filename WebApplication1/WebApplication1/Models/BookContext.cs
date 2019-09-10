7298288d3c11628e2463b32f87d4f44222c48ec1
Tiran Araqelyan <tirans3777@gmail.com>
2019-09-10 11:37:20 +0400

    add Dbcontext

---
+++ WebApplication1/WebApplication1/Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    
+        public class BookContext : DbContext
+        {
+            public DbSet<Book> Books { get; set; }
+        }
+    
+}
