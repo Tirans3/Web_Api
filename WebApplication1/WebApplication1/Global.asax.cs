7f442af82c2cfeccf043dab9b3528108e413329e
Tiran Araqelyan <tirans3777@gmail.com>
2019-09-11 11:31:42 +0400

    add new packages for ajax requests other domain

--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApiContrib.Formatting.Jsonp;
 using WebApplication1.Models;
 
 namespace WebApplication1
@@ -16,7 +17,7 @@ namespace WebApplication1
         protected void Application_Start()
         {
             Database.SetInitializer(new BookDbInitializer());
-
+            GlobalConfiguration.Configuration.AddJsonpFormatter();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
